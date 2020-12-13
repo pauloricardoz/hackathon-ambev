607f81cd9de1441e6421e44caf4e87f2d3a059d9
Rafael Assad <rafaassadeng@gmail.com>
2020-12-13 11:44:46 -0300

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,45 @@ class Footer extends React.Component {
     return (
       <footer className="footer">
         <p>©Copyright 2020</p>
-        <p>Project developed 
-        by <a href="https://www.linkedin.com/in/araujoluiz/" target="_blank" rel="noopener noreferrer">Luiz Araujo</a>,
-        <a href="https://www.linkedin.com/in/matheus-beaumord/" target="_blank" rel="noopener noreferrer"> Matheus Beaumord</a>
-        <a href="https://www.linkedin.com/in/rafaelassad/" target="_blank" rel="noopener noreferrer"> Rafael Assad</a> &
-        <a href="https://www.linkedin.com/in/paulo-ricardo-zambelli-taveira/" target="_blank" rel="noopener noreferrer"> Paulo Ricardo Zambelli</a>
+        <p>
+          Project developed by{' '}
+          <a href="https://www.linkedin.com/in/cduessel/" target="_blank" rel="noopener noreferrer">
+            {' '}
+            Christian Düssel
+          </a>
+          <a
+            href="https://www.linkedin.com/in/araujoluiz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Luiz Araujo
+          </a>
+          ,
+          <a
+            href="https://www.linkedin.com/in/matheus-beaumord/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {' '}
+            Matheus Beaumord
+          </a>
+          <a
+            href="https://www.linkedin.com/in/paulo-ricardo-zambelli-taveira/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {' '}
+            Paulo Ricardo Zambelli
+          </a>
+          &
+          <a
+            href="https://www.linkedin.com/in/rafaelassad/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {' '}
+            Rafael Assad
+          </a>{' '}
         </p>
       </footer>
     );

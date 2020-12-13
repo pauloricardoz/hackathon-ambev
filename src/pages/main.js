a26cebb79501d71598b71073c26d045bc0d12850
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-12-13 12:46:22 -0300

    Main com logo, titulo e descrição

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import fotoBanner from '../images/fotoBanner.png';
 
 const Main = () => {
-  return <main>MAIN Page</main>;
+  return (
+    <div>
+      <div>
+        <div>
+          <img alt="Banner Principal" src={fotoBanner} />
+        </div>
+        <h1>
+          Seu abastecimento de bebidas nunca foi tão simples, fácil e intuitivo
+        </h1>
+        <p> 
+          Uma preocupação a menos para o seu evento!
+        </p>
+        <p>
+          Clique em COMEÇAR e fassa agente seu pedido
+        </p>
+        <button type="button">
+          <Link to={`/agendamento`}>
+            Começar!
+          </Link>
+        </button>
+      </div>
+    </div>
+  )
 };
 
 export default Main;

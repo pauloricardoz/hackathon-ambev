ea912198e4f32c26fa82e40e7047baddb91ae0e2
pauloricardoz <trybe.przt@gmail.com>
2020-12-13 16:21:06 -0300

    forms pagamento

--- src/pages/pagamento.js
+++ src/pages/pagamento.js
@@ -10,17 +10,30 @@ function Pagamento() {
         <label htmlFor="first">Primeiro nome:</label>
         <input name="first" />
 
-        <label htmlFor="last">Ultimo nome:</label>
+        <label htmlFor="last">Último nome:</label>
         <input name="last" />
 
-        
+        <label htmlFor="street">Lougradouro:</label>
+        <input name="street" defaultValue={city.street} />
+
+        <label htmlFor="neighborhood">Bairro:</label>
+        <input name="neighborhood" defaultValue={city.neighborhood} />
+
+        <label htmlFor="number">Bairro:</label>
+        <input type="number" name="number" />
+
+        <label htmlFor="city">Cidade:</label>
+        <input name="city" defaultValue={city.city} />
+
+        <label htmlFor="state">Estado:</label>
+        <input name="state" defaultValue={city.state} />
       </form>
       <form>
         <label>Forma de pagamento:</label>
         <select onClick={(e) => setMetod(e.target.value)}>
-          <option value="credit">Cart'ao de cr[edito</option>
+          <option value="credit">Cartão de crédito</option>
           <option value="boleto">Boleto</option>
-          <option value="transfer">Transferencia</option>
+          <option value="transfer">Transferência</option>
           <option value="pix">Pix</option>
         </select>
       </form>

ca248ca055957a7eb1310df6762e678946963f32
Rafael Assad <rafaassadeng@gmail.com>
2020-12-13 10:38:01 -0300

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <p>©Copyright 2020</p>
+        <p>Project developed 
+        by <a href="https://www.linkedin.com/in/araujoluiz/" target="_blank" rel="noopener noreferrer">Luiz Araujo</a>,
+        <a href="https://www.linkedin.com/in/matheus-beaumord/" target="_blank" rel="noopener noreferrer"> Matheus Beaumord</a>
+        <a href="https://www.linkedin.com/in/rafaelassad/" target="_blank" rel="noopener noreferrer"> Rafael Assad</a> &
+        <a href="https://www.linkedin.com/in/paulo-ricardo-zambelli-taveira/" target="_blank" rel="noopener noreferrer"> Paulo Ricardo Zambelli</a>
+        </p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
